5fd35a4b337ca89ba04598e8ade4c651222ce714
Gonzalo <Gonco1@live.com>
2022-07-09 10:09:08 -0700

    Added more instructions/steps, removed reliance of api key, new meta.csv file which holds id-tags mapping

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# husbando_reborn
+# Husbando Reborn #
 
-## Next Steps :
-* Set up Danbooru API and get .env file setup
+## Next Steps ##
+* ~~Set up Danbooru API and get .env file setup~~ Turns out we don't need API
 * Run danbooru_image_dowloader.py until until there are ~10000 images
 * refactor anime_face_detection.ipynb to work as a script to process all the images
 * Resize all the cropped images to be the same (let's try 256 for now)
+* Use Danbooru API to search by image id for tags and save them to a csv for a id and tags grouping
+* Think of ways we can display data with the tags and categorize them(?)
+
+### Data Storage ###
+I expect the images (20,000 in total) to be about !15GB in total, so will need to think of a way to store it on the cloud but also be easily accessible for training on google colab since we will need a GPU for it.
+
+### Data Cleaning ###
+* Work on improving our selection process (picking the write tags). Maybe our metric for tracking this can be "how many images do we throw out when manually searching a random sample of 100 images?
+* Crop images around the face, resize images
+
+### Model Training ###
+We're not here yet, but we will be using [stylegan2](https://github.com/NVlabs/stylegan2) with tensorflow. I think pyTorch would be more efficient, but I'm more comfortable with tensorflow. We will also be using GPU's for training because we won't have to pay $100 a month for cloud services lmao.
+
--- danbooru_image_downloader.py
+++ danbooru_image_downloader.py
@@ -3,28 +3,35 @@ from pybooru import Danbooru
 import os
 import urllib.request
 import time
+from csv import writer
 
-#works only on my colab right now, will need to get it to work with .env files
-
+#helper funciton to write data to csv
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+        
 # https://pybooru.readthedocs.io/en/stable/api_danbooru.html#pybooru.api_danbooru.DanbooruApi_Mixin.post_list
-
-booru = Danbooru('danbooru',os.getenv('USER'), os.getenv('KEY'))
+booru = Danbooru('danbooru')
 posts = booru.post_list(limit = 100,random=True)
-
-#let's search through tag_string for 'boy'
 
 #let's do a while loop while our designated image file has less than 10,000 images
 
-dir_path = '/content'
+#directory we want to save images
+dir_path = 'data/uncropped/'
 number_of_images = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))])
 
 # I should figure out how to search for a list of strings in a string... hmm that explanation doesn't sound write
 
 total_images = 10000 #total images we want
 while number_of_images < total_images:
+  print(f'There are {number_of_images} images')
   posts = booru.post_list(limit = 100,random=True)
   for post in posts:
-    #lazy way of searching... we can do better (actually maybe this is something you can try to optimize)
+    #lazy way of searching... we can do better (actually maybe this is something Jess optimize so it's only 1 line of code, maybe like  (for word in words not in post['tag_string']) or something, I'm not sure)
     if ('1boy' in (post['tag_string'])) and \
     ('male_focus' in (post['tag_string'])) and \
     ('1girl' not in post['tag_string']) and \
@@ -32,15 +39,27 @@ while number_of_images < total_images:
     ('3girl' not in post['tag_string']) and \
     ('groin' not in post['tag_string']) and \
     ('bulge' not in post['tag_string']) and \
+    ('upskirt' not in post['tag_string']) and \
+    ('panties' not in post['tag_string']) and \
+    ('skirt' not in post['tag_string']) and \
+    ('grabbing' not in post['tag_string']) and \
     ('4girl' not in post['tag_string']):
       try:
         #download image
-        urllib.request.urlretrieve(post['file_url'],filename= f"/content/{post['id']}.png")
-        # print(post['tag_string'])
-        print(f'Image saved to /content/{post["id"]}.png')
+        urllib.request.urlretrieve(post['file_url'],filename= f"{dir_path}/{post['id']}.png")
+        # print(post['id'],post['tag_string'])
+        print(f'Image saved to {dir_path}{post["id"]}.png')
+        
+        #save tag and id to csv
+        row_contents = [post['id'],post['tag_string']]
+        append_list_as_row('meta.csv',row_contents)  
       except Exception as e:
         print(e)
-  print('Done searching... Waiting')
-  #re-search after 30 seconds
-  time.sleep(30)
+  print('Finished Downloading and Saving...')
+    
+  #re-search after 5 seconds
+  time.sleep(5)
   print('Searching....')
+  
+  #get number of images in directory
+  number_of_images = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))])
